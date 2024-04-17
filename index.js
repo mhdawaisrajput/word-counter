fc29f9bb09263d4fad67c313064157801b4c81a9
Muhammad Awais <162321104+mhdawaisrajput@users.noreply.github.com>
2024-04-17 16:12:29 +0500

    updated

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const results = await inquirer.prompt([
+    {
+        name: "paragraph",
+        message: "Please enter your paragraph to count the words !",
+        type: "input",
+    }
+]);
+const words = results.paragraph.trim().split(" ");
+console.log(words);
+console.log(chalk.magentaBright.bold(`Word count of your paragraph is ${words.length}`));
